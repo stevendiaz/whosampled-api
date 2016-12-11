dbc9ae6e51386d1f665f0e4b02788246b3eea02c
stevendiaz <stevendiaz@utexas.edu>
2016-12-11 13:31:17 -0600

    initial section parse logic sketch

--- parse.rb
+++ parse.rb
@@ -1,13 +1,30 @@
 require 'nokogiri'
 
+def parse_section(section)
+    a_tags = section.css('a')
+    # songs are in an array separated by blanks 
+    # the first entry after the blank will be the song name
+    # the following entries will be artists
+    result_json = []
+    counter = 0
+    for a_tag in a_tags do
+        if a_tag.text == ''
+            # add list to result json
+            # reset counter
+            counter = 0
+        else if counter == 0
+            # create new dict
+            song = a_tag.text
+        else
+            artist = a_tag.text
+    end
+    return a_tags.length
+end
 
 if __FILE__ == $0
     puts 'Hello...'
     html_doc = Nokogiri::HTML(File.open("html/3500-section.html"))
-    #spans = html_doc.css('span')
-    #for span in spans do
-    #    puts span.text
-    #end 
+    puts parse_section(html_doc)
     as = html_doc.css('a')
     for a in as do
         if a.text == '' 
